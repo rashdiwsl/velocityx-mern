94257a608ac3bbfc00c0386edeac94b95526ad2b
Rashmi Rathnayake <148036626+rashdiwsl@users.noreply.github.com>
2026-03-31 17:24:01 +0530

    feat(backend): updated server according to new api calls

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import carRoutes from "./routes/carRoutes.js";
 import garageRoutes from "./routes/garageRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -18,5 +19,6 @@ app.get("/", (req, res) => {
 app.use("/api/cars", carRoutes);
 app.use("/api/garage", garageRoutes);
 
+app.use("/api/auth", authRoutes);
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
