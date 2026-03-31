2852c5ed108cd7a3340a5b406dc873295a7b2794
Rashmi Rathnayake <148036626+rashdiwsl@users.noreply.github.com>
2026-03-31 17:17:30 +0530

    feat(frontend): implement api services

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; // your backend URL
+const API = "http://localhost:5000/api";
 
-export const getCars = async () => {
-  try {
-    const res = await axios.get(`${API_URL}/cars`);
-    return res.data;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
+export const getCars = () => axios.get(`${API}/cars`);
+export const getGarage = () => axios.get(`${API}/garage`);
+export const addToGarage = (id) =>
+  axios.post(`${API}/garage/add`, { carId: id });
+export const removeFromGarage = (id) =>
+  axios.delete(`${API}/garage/${id}`);
