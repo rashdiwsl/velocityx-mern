c8f9297e74c0b044a7721c38c3d6d4a4c5d101b0
Rashmi Rathnayake <148036626+rashdiwsl@users.noreply.github.com>
2026-03-31 17:27:54 +0530

    feat(frontend): add route paths for login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GaragePage from "./pages/GaragePage";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/garage" element={<GaragePage />} />
+        <Route path="/login" element={<LoginPage />} />
+<Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
