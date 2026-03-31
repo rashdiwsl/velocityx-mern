49f60531b9e207962f2f7a404fc264549cfc5530
Rashmi Rathnayake <148036626+rashdiwsl@users.noreply.github.com>
2026-03-31 17:23:27 +0530

    feat(backend): created User model and auth routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,41 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    name,
+    email,
+    password: hashed,
+  });
+
+  await user.save();
+  res.json(user);
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) return res.status(400).json({ message: "Wrong password" });
+
+  const token = jwt.sign({ id: user._id }, "secret123");
+
+  res.json({ token, user });
+});
+
+export default router;
